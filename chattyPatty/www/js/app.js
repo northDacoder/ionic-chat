2a1739857fc030dc33cae1c575e7c9bcc3de9046
John Ryan <northdacoder@gmail.com>
2015-02-14 09:57:19 -0600

    add dependencies for firebase + angularfire

--- chattyPatty/www/js/app.js
+++ chattyPatty/www/js/app.js
@@ -5,9 +5,16 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('starter', [
 
-.run(function($ionicPlatform) {
+            'ionic',
+            'firebase',
+            'starter.controllers',
+            'starter.services'
+
+])
+
+.run(function($ionicPlatform, $rootScope) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -17,6 +24,9 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
+    }
+    $rootScope.logout = function() {
+        console.log('Logging out from the app');
     }
   });
 })
@@ -29,74 +39,105 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   // Each state's controller can be found in controllers.js
   $stateProvider
 
+  .state('login', function() {
+      url: "/login",
+      templateUrl: "templates/login.html",
+      controller: "LoginCtrl"
+  })
+
+  .state('tab', {
+      url: "/tab",
+      abstract: true,
+      templateUrl: "templates/tabs.html"
+  })
+
+  .state('tab.rooms', {
+      url: "/rooms",
+      views: {
+          'tab-rooms': {
+              templateUrl: 'templates/tab-rooms.html',
+              controller: 'RoomsCtrl'
+          }
+      }
+  })
+
+  .state('tab.chat', {
+      views: {
+          templateUrl: 'templates/tab-chat.html',
+          controller: 'ChatCtrl'
+      }
+  })
+
+  $urlRouterProvider.otherwise('/login');
+
   // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: "/tab",
-    abstract: true,
-    templateUrl: "templates/tabs.html"
-  })
-
-  // Each tab has its own nav history stack:
-
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
-
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
-
-  .state('tab.friends', {
-      url: '/friends',
-      views: {
-        'tab-friends': {
-          templateUrl: 'templates/tab-friends.html',
-          controller: 'FriendsCtrl'
-        }
-      }
-    })
-    .state('tab.friend-detail', {
-      url: '/friend/:friendId',
-      views: {
-        'tab-friends': {
-          templateUrl: 'templates/friend-detail.html',
-          controller: 'FriendDetailCtrl'
-        }
-      }
-    })
-
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  });
-
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+// .state('tab', {
+//     url: "/tab",
+//     abstract: true,
+//     templateUrl: "templates/tabs.html"
+//   })
+//
+//   // Each tab has its own nav history stack:
+//
+//   .state('tab.dash', {
+//     url: '/dash',
+//     views: {
+//       'tab-dash': {
+//         templateUrl: 'templates/tab-dash.html',
+//         controller: 'DashCtrl'
+//       }
+//     }
+//   })
+//
+//   .state('tab.chats', {
+//       url: '/chats',
+//       views: {
+//         'tab-chats': {
+//           templateUrl: 'templates/tab-chats.html',
+//           controller: 'ChatsCtrl'
+//         }
+//       }
+//     })
+//     .state('tab.chat-detail', {
+//       url: '/chats/:chatId',
+//       views: {
+//         'tab-chats': {
+//           templateUrl: 'templates/chat-detail.html',
+//           controller: 'ChatDetailCtrl'
+//         }
+//       }
+//     })
+//
+//   .state('tab.friends', {
+//       url: '/friends',
+//       views: {
+//         'tab-friends': {
+//           templateUrl: 'templates/tab-friends.html',
+//           controller: 'FriendsCtrl'
+//         }
+//       }
+//     })
+//     .state('tab.friend-detail', {
+//       url: '/friend/:friendId',
+//       views: {
+//         'tab-friends': {
+//           templateUrl: 'templates/friend-detail.html',
+//           controller: 'FriendDetailCtrl'
+//         }
+//       }
+//     })
+//
+//   .state('tab.account', {
+//     url: '/account',
+//     views: {
+//       'tab-account': {
+//         templateUrl: 'templates/tab-account.html',
+//         controller: 'AccountCtrl'
+//       }
+//     }
+//   });
+//
+//   // if none of the above states are matched, use this as the fallback
+//   $urlRouterProvider.otherwise('/tab/dash');
 
 });
--- chattyPatty/www/js/controllers.js
+++ chattyPatty/www/js/controllers.js
@@ -1,28 +1,59 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  }
+.controller('LoginCtrl', function ($scope, $ionicModal, $state) {
+
+    console.log('Login Controller Initialized');
+
+    $ionicModal.fromTemplateUrl('templates/signup.html', {
+        scope: $scope
 })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
-
-.controller('FriendsCtrl', function($scope, Friends) {
-  $scope.friends = Friends.all();
-})
-
-.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
-  $scope.friend = Friends.get($stateParams.friendId);
-})
-
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+.then(function (modal) {
+    $scope.modal = modal;
 });
+
+$scope.createUser = function (user) { }
+$scope.signIn = function () {
+    $state.go('tab.rooms'); }
+})
+
+.controller('ChatCtrl', function ($scope, Chats) {
+    console.log("Chat Controller initialized");
+    $scope.chats = Chats.all();
+})
+
+.controller('RoomsCtrl', function ($scope, Rooms) {
+    console.log("Rooms Controller initialized");
+    $scope.rooms = Rooms.all();
+});
+
+
+// Read more at https://www.airpair.com/angularjs/posts/build-a-real-time-hybrid-app-with-ionic-firebase#BiCSVeZ0F4wDioLW.99
+
+// .controller('DashCtrl', function($scope) {})
+//
+// .controller('ChatsCtrl', function($scope, Chats) {
+//   $scope.chats = Chats.all();
+//   $scope.remove = function(chat) {
+//     Chats.remove(chat);
+//   }
+// })
+//
+// .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+//   $scope.chat = Chats.get($stateParams.chatId);
+// })
+//
+// .controller('FriendsCtrl', function($scope, Friends) {
+//   $scope.friends = Friends.all();
+// })
+//
+// .controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
+//   $scope.friend = Friends.get($stateParams.friendId);
+// })
+//
+// .controller('AccountCtrl', function($scope) {
+//   $scope.settings = {
+//     enableFriends: true
+//   };
+// });
